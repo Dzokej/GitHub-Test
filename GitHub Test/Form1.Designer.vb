11bd5e5e8932cb9a18b47585e95ab060282777dc
Itachi <Itachi@LAPTOP-9MV3EQH4>
2023-03-02 10:35:27 -0600

    version 2

--- GitHub Test/Form1.Designer.vb
+++ GitHub Test/Form1.Designer.vb
@@ -22,42 +22,97 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.lblOoutput = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.picPortrait = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.picPortrait, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnTest
         '
-        Me.Button1.Location = New System.Drawing.Point(279, 78)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(173, 70)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnTest.Location = New System.Drawing.Point(279, 78)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(173, 70)
+        Me.btnTest.TabIndex = 0
+        Me.btnTest.Text = "Button1"
+        Me.btnTest.UseVisualStyleBackColor = True
         '
-        'Label1
+        'lblOoutput
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(349, 239)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 16)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
+        Me.lblOoutput.AutoSize = True
+        Me.lblOoutput.Location = New System.Drawing.Point(349, 239)
+        Me.lblOoutput.Name = "lblOoutput"
+        Me.lblOoutput.Size = New System.Drawing.Size(48, 16)
+        Me.lblOoutput.TabIndex = 1
+        Me.lblOoutput.Text = "Label1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewFileToolStripMenuItem, Me.OpenFileToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewFileToolStripMenuItem
+        '
+        Me.NewFileToolStripMenuItem.Name = "NewFileToolStripMenuItem"
+        Me.NewFileToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.NewFileToolStripMenuItem.Text = "New File"
+        '
+        'OpenFileToolStripMenuItem
+        '
+        Me.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem"
+        Me.OpenFileToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.OpenFileToolStripMenuItem.Text = "Open File"
+        '
+        'picPortrait
+        '
+        Me.picPortrait.Location = New System.Drawing.Point(504, 175)
+        Me.picPortrait.Name = "picPortrait"
+        Me.picPortrait.Size = New System.Drawing.Size(232, 197)
+        Me.picPortrait.TabIndex = 3
+        Me.picPortrait.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.picPortrait)
+        Me.Controls.Add(Me.lblOoutput)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "GitHub Test"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.picPortrait, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents btnTest As Button
+    Friend WithEvents lblOoutput As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents picPortrait As PictureBox
 End Class
--- GitHub Test/Form1.vb
+++ GitHub Test/Form1.vb
@@ -1,7 +1,9 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Label1.Text = "This works."
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        lblOoutput.Text = "This works."
+
+        lblOoutput.Text = "New version."
     End Sub
 End Class
