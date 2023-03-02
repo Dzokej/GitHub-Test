dea3b0c0ed200da6fab49f64f9aa53c7d13016c1
Itachi <Itachi@LAPTOP-9MV3EQH4>
2023-03-02 10:17:13 -0600

    Add project files.

---
+++ GitHub Test/Form1.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "This works."
+    End Sub
+End Class
